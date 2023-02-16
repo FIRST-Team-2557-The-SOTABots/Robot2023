2a271812d3720aecf93c818380a51a3e8a715e59
BigJonas <jonasgaribay012@gmail.com>
2023-02-16 15:05:09 -0800

    Changed how rotating around a module closest to a point is handled

--- src/main/java/frc/robot/Commands/RotateAroundModuleClosestToPoint.java
+++ src/main/java/frc/robot/Commands/RotateAroundModuleClosestToPoint.java
@@ -8,15 +8,16 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Subsystems.Swerve.ShiftingSwerveDrive;
 
-public class RotateAroundFarthestModule extends DefaultDrive{
+public class RotateAroundModuleClosestToPoint extends DefaultDrive{
   private ShiftingSwerveDrive mSwerveDrive;
   private Translation2d[] mModuleTranslations;
-  private Translation2d mFarthestModule;
+  private Translation2d mClosestModule;
+  private Translation2d mPoint;
 
   private CommandXboxController mDriveStick;
 
   /** Creates a new RotateAroundFarthestModule. */
-  public RotateAroundFarthestModule(ShiftingSwerveDrive swerveDrive, CommandXboxController driveStick) {
+  public RotateAroundModuleClosestToPoint(ShiftingSwerveDrive swerveDrive, CommandXboxController driveStick, Translation2d point) {
     // Use addRequirements() here to declare subsystem dependencies.
     super(swerveDrive, driveStick);
     mModuleTranslations = mSwerveDrive.getModuleTranslation();
@@ -26,12 +27,10 @@ public class RotateAroundFarthestModule extends DefaultDrive{
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // Arbitrarily large number down field for distance calculation doesnt matter how large it is I just choes a number
-    Translation2d downField = new Translation2d(999.0, 0.0);
-    mFarthestModule = new Translation2d();
+    mClosestModule = new Translation2d();
     for (int i = 0; i < mModuleTranslations.length; i++) {
-      if (mModuleTranslations[i].getDistance(downField) < mFarthestModule.getDistance(downField)) {
-        mFarthestModule = mModuleTranslations[i];
+      if (mModuleTranslations[i].getDistance(mPoint) < mClosestModule.getDistance(mPoint)) {
+        mClosestModule = mModuleTranslations[i];
       }
     }
   }
@@ -43,7 +42,7 @@ public class RotateAroundFarthestModule extends DefaultDrive{
     double str = mDriveStick.getLeftX();
     double rot = mDriveStick.getRightX();
 
-    super.drive(fwd, str, rot, mSwerveDrive.getRotation2d(), mFarthestModule);
+    super.drive(fwd, str, rot, mSwerveDrive.getRotation2d(), mClosestModule);
   }
 
   // Called once the command ends or is interrupted.
